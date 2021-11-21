0d655db8aea46596a76fbfc37c3f665121e6a57b
NChervinska <chervinska777@gmail.com>
2021-11-21 23:28:40 +0200

    Merge

--- src/components/auth-nav.js
+++ src/components/auth-nav.js
@@ -7,18 +7,25 @@ import Modal from '../modal_dialog/modal';
 const AuthNav = () => {
     const [modalLoginActive, setModalLoginActive] = useState(true);
     const [modalRegistrationActive, setModalRegistrationActive] = useState(false);
-    return (
-        <div className="navbar-nav ml-auto">
-            <button className='openm' onClick={() => setModalLoginActive(true)}>Login</button>
-            <Modal active={modalLoginActive} setActive={setModalLoginActive}>
-                <Login/>
-            </Modal>
-            <button className='openm' onClick={() => setModalRegistrationActive(true)}>Registration</button>
-            <Modal active={modalRegistrationActive} setActive={setModalRegistrationActive}>
-                <Registration/>
-            </Modal>
-        </div>
-    );
+    const token = localStorage.getItem("token"); 
+    const isAuth = token != null; 
+    if(!isAuth) {
+        return (
+            <div>
+                <button className='open-log' onClick={() => setModalLoginActive(true)}>Login</button>
+                <Modal active={modalLoginActive} setActive={setModalLoginActive}>
+                    <Login></Login>
+                </Modal> 
+                <button className='open' onClick={() => setModalRegistrationActive(true)}>Registration</button>
+                <Modal active={modalRegistrationActive} setActive={setModalRegistrationActive}>
+                    <Registration></Registration>
+                </Modal> 
+            </div>
+        ); 
+    }
+    else{
+        return (<button onClick={() => localStorage.removeItem("token")}>Logout</button> );
+    }
 };
 
 export default AuthNav;
